d1427dfe613481b93f5db095e29ab951ba4bac67
betelmatnas-maker <betel.matnas@gmail.com>
2026-09-02 18:19:07 +0300

    Delete api directory

--- api/auth/logout.js
+++
@@ -1,8 +0,0 @@
-const { clearSessionCookie } = require("../../../lib/auth");
-
-module.exports = async function handler(req, res) {
-  res.setHeader("Set-Cookie", clearSessionCookie());
-  res.status(200).json({ ok: true });
-};
-
-module.exports.default = module.exports;
